630d199e9eec70654797031d69f3190c9f6f3a85
chuanyu526 <yuchuan0615@hotmail.com>
2014-02-28 23:06:16 -0800

    sort

---
+++ Sorting.java
@@ -0,0 +1,65 @@
+import java.util.*;
+public class Sorting{
+
+
+
+
+
+/*
+
+	public static void mergeSort(  ){
+
+
+
+
+
+
+
+	}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main(String [] args){
+		int[] a = {7,2,5,4,3,1,8,9,0,6};
+		/*
+		List<Integer> aList = new ArrayList<Integer>();
+		aList.add(new Integer(1));
+		System.out.print(aList.get(0).intValue());
+		*/
+		
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+}
