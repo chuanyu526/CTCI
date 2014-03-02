59bff826665bcdce99aa7325e46c8ba762b64e3d
chuanyu526 <yuchuan0615@hotmail.com>
2014-03-01 23:23:43 -0800

    quicksort

--- Sorting.java
+++ Sorting.java
@@ -5,7 +5,7 @@ public class Sorting{
 
 
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public static int[] mergeSort( int [] a){
 		int l = a.length; 
@@ -17,8 +17,6 @@ public class Sorting{
 		}
 	}
 
-
-    
     // Returns the first half of the given array.
     public static int[] leftHalf(int[] array) {
         int size1 = array.length / 2;
@@ -40,30 +38,7 @@ public class Sorting{
         return right;
     }
     
-
-
-/*
-	protected void merge(int [] x, int [] y ){ 
-		if (x.length == 0){
-			return y;
-		}
-		if(y.length == 0){
-			return x;
-		}
-		if(x[0]>=y[0]){
-			int l = x.length + y.length;
-		    int[] a = new int[l]; 
-		    a[0] = x[0];
-			for(int i =0; i<l; i++){
-				a[]
-			}
-		}
-
-	}
-*/
-
-
-
+    // Merge two sorted arrays.
 	protected static int[] merge(int [] x, int [] y ){ 
 		int x_pos =0;
 		int y_pos =0; 
@@ -87,27 +62,74 @@ public class Sorting{
 					x_pos++;
 				}
 			}
-
 		}
 		return comb; 
 	}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static void quickSort(int[] a, int left, int right){
+		if(right > left ){
+			int pivot = (left + right)/2;
+			int newPivot = partition(a,left,right,pivot);
+			quickSort(a, left, newPivot-1);
+			quickSort(a, newPivot+1, right);
+		}
+	}
 
 
+	public static int partition(int[] a, int left, int right, int pivot){
+		int pivotValue = a[pivot];
+		int holder;
+		int storeIndex = left;
+		a[pivot] = a[right];
+		a[right] = pivotValue;
+		for(int i=left; i<right; i++){
+			if(a[i] <= pivotValue){
+				holder = a[i];
+				a[i] = a[storeIndex];
+				a[storeIndex] = holder;
+				storeIndex++; 
+			}
+		}
+		holder = a[storeIndex];
+		a[storeIndex] = a[right];
+		a[right] = holder; 
+		return storeIndex;
+	} 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static void selectionSort(int[] a){
+
+		int length = a.length;
+		int holder; 
+		for(int j = 0; j < length; j++){
+			for(int i = j; i < length; i++) {
+				if( a[i] > a[j] ){
+					holder = a[i];
+					a[i] = a[j];
+					a[j] = holder;
+				}
+			} 
+		}
+	}
+
+	public static void bubbleSort(int[] a) {
+		int length = a.length;
+		int holder;
+		for (int i = 0; i < length; i++) { // already sorted
+			for (int j = 0; j < length - i - 1; j++) { // walk through
+				if (a[j] > a[j + 1]) {
+					holder = a[j];
+					a[j] = a[j + 1];
+					a[j+1] = holder;					
+				}
+			}
+		}
+
+	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 	public static void main(String [] args){
 		int[] a = {7,2,5,4,3,1,8,9,0,6};
 		/*
@@ -119,6 +141,11 @@ public class Sorting{
 
         int[] sorted_a = mergeSort(a);
         System.out.print(Arrays.toString(sorted_a));
+
+        //quickSort(a,0,9);
+        bubbleSort(a);
+        System.out.print(Arrays.toString(a)); 
+
 		
 
 
